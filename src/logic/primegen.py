509a93ec10b5f24d16003c876e9917d68f556f92
katajak <101673344+katajak@users.noreply.github.com>
2023-02-07 17:36:08 +0200

    nimien parannus

--- src/logic/primegen.py
+++ src/logic/primegen.py
@@ -82,16 +82,16 @@ class AlkulukuGeneraattori:
         """Metodi, joka generoi pieniä alkulukua lukuun n asti Eratostheneen seulalla.
             Palauttaa listan alkuluvuista n asti
         """
-        prime = [True for _ in range(n+1)]
-        p = 2
-        while p*p <= n:
-            if prime[p]:
-                for i in range(p*p, n+1, p):
-                    prime[i] = False
-            p += 1
+        alkulukulista = [True for _ in range(n+1)]
+        a_luku = 2
+        while a_luku*a_luku <= n:
+            if alkulukulista[a_luku]:
+                for i in range(a_luku*a_luku, n+1, a_luku):
+                    alkulukulista[i] = False
+            a_luku += 1
 
         alkuluvut = []
-        for p in range(2, n+1):
-            if prime[p]:
-                alkuluvut.append(p)
+        for a_luku in range(2, n+1):
+            if alkulukulista[a_luku]:
+                alkuluvut.append(a_luku)
         return alkuluvut
