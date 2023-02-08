41712953fdc650d6fc8a82ced0609fe6534ac5ec
katajak <101673344+katajak@users.noreply.github.com>
2023-02-08 15:39:13 +0200

    suurin yhteinen tekijä metodi

--- src/logic/keygen.py
+++ src/logic/keygen.py
@@ -7,3 +7,14 @@ class AvainGeneraattori:
     def generoi_avaimet(self, bittimaara):
         p, q = self.alkulukugeneraattori.generoi_alkuluvut(bittimaara)
         n = p*q
+
+    def syt(self, p, q):
+        """https://en.wikipedia.org/wiki/Euclidean_algorithm#Implementations
+        """
+        if q == 0:
+            return p
+        else:
+            return self.syt(q, p%q)
+
+    def carmichaelin_funktio(self, p, q):
+        pass
