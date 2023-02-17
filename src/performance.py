be0d87c3619ccdfaebc6baadf561f7ff941c1bb2
katajak <101673344+katajak@users.noreply.github.com>
2023-02-17 16:46:41 +0200

    korjattu suorituskykytesti

--- src/performance.py
+++ src/performance.py
@@ -11,27 +11,32 @@ def main():
     print("Kaikissa testeissä lasketaan keskiarvo 20:ltä suorituskerralta.")
     avaingeneraattori = AvainGeneraattori((AlkulukuGeneraattori(SatunnaislukuGeneraattori())),
                                            Avaimenpera(TiedostonKasittelija()))
-    testitiedosto = NamedTemporaryFile(encoding="utf-8", mode="w+")
+    testi1024_yks = NamedTemporaryFile(encoding="utf-8", mode="w+")
+    testi1024_jul = NamedTemporaryFile(encoding="utf-8", mode="w+")
     ajat = []
     for _ in range(20):
         alku = time.time()
-        avaingeneraattori.generoi_avaimet(1024, "testi1024", testitiedosto.name)
+        avaingeneraattori.generoi_avaimet(1024, "testi1024", testi1024_yks.name, testi1024_jul.name)
         loppu = time.time()
         ajat.append(loppu-alku)
     print(f"1024 bit: {sum(ajat)/len(ajat)} sekuntia")
 
+    testi2048_yks = NamedTemporaryFile(encoding="utf-8", mode="w+")
+    testi2048_jul = NamedTemporaryFile(encoding="utf-8", mode="w+")
     ajat = []
     for _ in range(20):
         alku = time.time()
-        avaingeneraattori.generoi_avaimet(2048, "testi2048", testitiedosto.name)
+        avaingeneraattori.generoi_avaimet(2048, "testi2048", testi2048_yks.name, testi2048_jul.name)
         loppu = time.time()
         ajat.append(loppu-alku)
     print(f"2048 bit: {sum(ajat)/len(ajat)} sekuntia")
 
+    testi4096_yks = NamedTemporaryFile(encoding="utf-8", mode="w+")
+    testi4096_jul = NamedTemporaryFile(encoding="utf-8", mode="w+")
     ajat = []
     for _ in range(20):
         alku = time.time()
-        avaingeneraattori.generoi_avaimet(4096, "testi4096", testitiedosto.name)
+        avaingeneraattori.generoi_avaimet(4096, "testi4096", testi4096_yks.name, testi4096_jul.name)
         loppu = time.time()
         ajat.append(loppu-alku)
     print(f"4096 bit: {sum(ajat)/len(ajat)} sekuntia")
